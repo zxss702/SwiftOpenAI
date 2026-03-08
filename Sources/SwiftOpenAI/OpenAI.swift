a02c74e34281650d39f9f230f36c42b91e99bb5d
知阳 <zxs20060621@icloud.com>
2026-03-08 11:41:57 +0800

    fix

--- Sources/SwiftOpenAI/OpenAI.swift
+++ Sources/SwiftOpenAI/OpenAI.swift
@@ -108,7 +108,11 @@ public class OpenAI {
                         try Task.checkCancellation()
                         guard !line.isEmpty, !line.hasPrefix(":") else { continue }
                         
-                        let dataString = line.replacingOccurrences(of: "data:", with: "").trimmingCharacters(in: .whitespaces)
+                        var dataString = line
+                        if dataString.hasPrefix("data:") {
+                            dataString.removeFirst(5)
+                        }
+                        dataString = dataString.trimmingCharacters(in: .whitespaces)
                         
                         if dataString == "[DONE]" {
                             continuation.finish()
